c199e1e3b6314033c1a63b9d8b96a1a0cb7d0a10
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-02 11:41:27 -0400

    include topic in the storage of quotes - still need to figure out the return random quote of topic

--- quotecollector.rb
+++ quotecollector.rb
@@ -2,11 +2,20 @@ require_relative 'multilinguist.rb'
 
 class QuoteCollector < Multilinguist
 
+  attr_reader :quote_collection
+
   def initialize
     super
     @quote_collection = []
   end
 
-  
+  def collect_quote(quote_text, topic_label)
+    @quote_collection << {"quote" => quote_text, "topic" => topic_label}
+  end
+
+  def random_quote(topic_label)
+    return say_in_local_language(quote_collection.find { |q| q["topic"] == topic_label }["quote"])
+  end
+
 
 end
