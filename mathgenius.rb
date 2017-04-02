347230025647e4b2d1cab13ae0aedde61535413f
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-02 09:55:38 -0400

    test and complete MathGenius class

--- mathgenius.rb
+++ mathgenius.rb
@@ -14,8 +14,15 @@ class MathGenius < Multilinguist
     else
       puts "please input an array of numbers"
     end
-    return say_in_local_language("The total is #{total}")
+    return say_in_local_language("The total is #{total  }")
   end
 
 
 end
+
+me = MathGenius.new
+puts me.add_total([23,45,676,34,5778,4,23,5465])
+me.travel_to("India")
+puts me.add_total([6,3,6,68,455,4,467,57,4,534])
+me.travel_to("Italy")
+puts me.add_total([324,245,6,343647,686545]) 
